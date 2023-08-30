56ceb28c361a7bb1b4c438d3e3249c96b8628fd3
Andrew Eubanks <aeubanksguitar@gmail.com>
2023-08-29 22:28:44 -0400

    took away links for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,6 @@ function Navbar() {
   return (
     <nav className='primary-nav row'>
       <h1>Solvher</h1>
-      <ul>
-        <li>
-          <a href='CountByWeight'>Solve a Partial</a>
-        </li>
-      </ul>
     </nav>
   );
 }
