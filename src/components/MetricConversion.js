71e12154a65073c8375d7e0de511766d51f6aa86
Andrew Eubanks <aeubanksguitar@gmail.com>
2023-09-11 22:57:43 -0400

    added some styling

--- src/components/MetricConversion.js
+++ src/components/MetricConversion.js
@@ -1,6 +1,6 @@
 import Card from '../shared/Card';
 import UnitData from '../data/UnitData';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function MetricConversion() {
   const [unit, setUnit] = useState(UnitData[0]);
@@ -8,6 +8,8 @@ function MetricConversion() {
   const [net, setNet] = useState(0);
   const [input, setInput] = useState(0);
   const [density, setDensity] = useState(1);
+  const inputRef = useRef(null);
+  const densityRef = useRef(null);
 
   const units = UnitData.map((unit) => <option key={unit.id}>{unit.id}</option>);
 
@@ -23,22 +25,30 @@ function MetricConversion() {
       case 'grams':
         answer = parseFloat(input / 1000).toFixed(3);
         setNet(answer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+        inputRef.current.value = null;
         break;
       case 'pounds':
         answer = parseFloat(input / 2.2046).toFixed(3);
         setNet(answer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+        inputRef.current.value = null;
         break;
       case 'liters':
         answer = parseFloat(input * density);
         setNet(answer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+        inputRef.current.value = null;
+        densityRef.current.value = null;
         break;
       case 'mililiters':
         answer = parseFloat((input * density) / 1000).toFixed(3);
         setNet(answer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+        inputRef.current.value = null;
+        densityRef.current.value = null;
         break;
       case 'gallons':
         answer = parseFloat(input * 3.7854 * density).toFixed(3);
         setNet(answer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+        inputRef.current.value = null;
+        densityRef.current.value = null;
         break;
       default:
         break;
@@ -54,10 +64,10 @@ function MetricConversion() {
       <h3>Convert a different unit into KGs</h3>
       <form onSubmit={solve}>
         <label htmlFor='input'>Weight of item</label>
-        <input type='text' name='input' onChange={(e) => setInput(+e.target.value)} inputMode='numeric' required />
+        <input type='text' name='input' onChange={(e) => setInput(+e.target.value)} inputMode='numeric' ref={inputRef} required />
         <select onChange={handleSelect}>{units}</select>
         {isVolume && <label htmlFor='density'>Density</label>}
-        {isVolume && <input type='text' name='density' onChange={(e) => setDensity(+e.target.value)} inputMode='numeric' required />}
+        {isVolume && <input type='text' name='density' onChange={(e) => setDensity(+e.target.value)} inputMode='numeric' ref={densityRef} required />}
         <label htmlFor='solve'>Solve</label>
         <button type='submit' name='solve'>
           Submit
